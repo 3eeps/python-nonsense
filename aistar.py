9a882ef3d611d82d1573d1b362a9a6ffdb2c3f45
3eeps <55860052+3eeps@users.noreply.github.com>
2023-04-03 03:12:12 +0000

    testing some astar mazes

---
+++ aistar.py
@@ -0,0 +1,107 @@
+# python-fun/perceptron.py
+
+# Author: Christian Careaga (christian.careaga7@gmail.com)
+# A* Pathfinding in Python (2.7)
+# Please give credit if used
+
+from heapq import *
+import numpy
+
+def heuristic(a, b):
+    return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
+
+def astar(array, start, goal):
+
+    neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
+
+    close_set = set()
+    came_from = {}
+    gscore = {start:0}
+    fscore = {start:heuristic(start, goal)}
+    oheap = []
+
+    heappush(oheap, (fscore[start], start))
+    
+    while oheap:
+
+        current = heappop(oheap)[1]
+
+        if current == goal:
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current]
+            return data
+
+        close_set.add(current)
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j            
+            tentative_g_score = gscore[current] + heuristic(current, neighbor)
+            if 0 <= neighbor[0] < array.shape[0]:
+                if 0 <= neighbor[1] < array.shape[1]:                
+                    if array[neighbor[0]][neighbor[1]] == 1:
+                        continue
+                else:
+                    # array bound y walls
+                    continue
+            else:
+                # array bound x walls
+                continue
+                
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                continue
+                
+            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+                came_from[neighbor] = current
+                gscore[neighbor] = tentative_g_score
+                fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
+                heappush(oheap, (fscore[neighbor], neighbor))
+                
+    return False
+
+def convert_path(path):
+    converted_path = []
+    if len(path) > 0:
+        for i in range(len(path) - 1):
+            converted_path.append((path[i+1][0] - path[i][0], path[i+1][1] - path[i][1]))
+    return converted_path
+
+def astar_path(current_map, current_pos, goal):
+    check_path = astar(current_map, current_pos, goal)
+    output_path = convert_path(check_path)
+    return output_path
+
+nmap = numpy.array([
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+nmap2 = numpy.array([
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0]])
+
+new_path = astar(nmap2, (0,0), (10,13))
+finished_path = convert_path(new_path)
+
+# print (astar(nmap, (0,0), (10,13)))
+if finished_path is True:
+    print (finished_path)
+else:
+    print ("No path found")
